ab01e164176aee741b63bc8e52cb06c19729b86e
Xiao Sa <sa.xiao@thoughtworks.com>
2023-02-01 14:27:09 +0800

    [E-17] add jest config

---
+++ jest.config.js
@@ -0,0 +1,25 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  // tsc 转译
+  preset: "ts-jest",
+  // 自动清除 Mock
+  clearMocks: true,
+  // 覆盖率
+  collectCoverage: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  // 覆盖路径
+  collectCoverageFrom: [
+    "<rootDir>/src/service/**/*.{tsx,ts}",
+    "<rootDir>/src/utils/**/*.{tsx,ts}",
+  ],
+  transform: {
+    ".+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$":
+      "jest-transform-stub",
+    "node_modules/axios/.+\\.(j|t)sx?$": "ts-jest",
+  },
+  transformIgnorePatterns: ["/node_modules/(?!(axios)/)"],
+  testEnvironment: "jsdom",
+
+  setupFilesAfterEnv: ["<rootDir>/test/setupTests.ts"],
+};
---
+++ test/App.test.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+test("Hello World", () => {
+  expect("Hello World").toEqual("Hello World");
+});
