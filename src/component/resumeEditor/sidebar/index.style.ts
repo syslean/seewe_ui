d699927c009e081fca588df8de9d7ad14a0dd30b
Xiao Sa <sa.xiao@thoughtworks.com>
2023-02-02 14:01:29 +0800

    [E-21] menu Item margin change

--- src/component/resumeEditor/sidebar/index.style.ts
+++ src/component/resumeEditor/sidebar/index.style.ts
@@ -1,10 +1,9 @@
 import styled from "styled-components";
 
 export const SideBarWrapper = styled.div`
-  padding-top: 1rem;
+  padding: 1rem 1rem 0 1rem;
   margin-right: 1rem;
   border-radius: 0.5rem;
   background-color: #F9F9F9;
-  min-width: 12rem;
-  max-width: 12rem;
+  width: 10rem;
 `
--- src/component/resumeEditor/sidebar/menuItem/index.style.ts
+++ src/component/resumeEditor/sidebar/menuItem/index.style.ts
@@ -4,7 +4,7 @@ export const MenuItemWrapper = styled.div`
   background: #FFFFFF;
   border-radius: 0.5rem;
   box-shadow: 0 2px 4px 0 rgba(197, 197, 197, 0.3);
-  margin: 0 1rem 0.75rem 1rem;
+  margin-bottom: 0.75rem;
   height: 2.6rem;
 
   display: flex;
@@ -17,7 +17,7 @@ export const MenuIcon = styled.div`
 `
 
 export const MenuTitle = styled.div`
-  font-family: "Open Sans",serif;
+  font-family: "Open Sans", serif;
   font-size: 0.875rem;
   font-weight: 600;
 
